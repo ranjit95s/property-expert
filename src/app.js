eeceb0f4ea582573f0161eb9eb7da017054fbf67
Ranjit kumar suryawanshi <djdj775257@gmail.com>
2021-04-24 23:24:49 +0530

    last try

--- src/app.js
+++ src/app.js
@@ -46,7 +46,7 @@ app.set('json spaces', '  ');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "./public/uploadsTemp/");
+        cb(null, "../public/uploadsTemp/");
     },
     filename: function (req, file, cb) {
         cb(
